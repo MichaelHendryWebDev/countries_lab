2f5eb99f62fab8a5aec0f4a5aaa94cfd3844226d
mike <>
2020-04-14 16:30:24 +0100

    lab can select country and see flag

--- src/app.js
+++ src/app.js
@@ -2,6 +2,27 @@ import Vue from 'vue';
 
 document.addEventListener('DOMContentLoaded', () => {
   new Vue({
-    el: '#app'
+    el: '#app',
+    data: {
+      flagImgURL: null,
+      countries: [],
+      selectedCountry: null
+    },
+    mounted(){
+      // this.fetchFlag();
+      this.fetchCountries();
+    },
+    methods: {
+      fetchFlag: function(){
+        fetch("https://restcountries.eu/rest/v2/all")
+          .then(response => response.json())
+          .then(data => this.flagImgURL = data[6].flag);
+      },
+      fetchCountries: function(){
+        fetch("https://restcountries.eu/rest/v2/all")
+        .then(countries => countries.json())
+        .then(data => this.countries = data)
+      }
+    }
   });
 });
